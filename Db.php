489c0d42e683dab52bb9c2b97b153a777c68fbf7
Amit Roy <uamitroy@gmail.com>
2021-12-18 16:57:07 +0530

    setup

---
+++ Db.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author      Amit Roy <amit@softflies.com>
+ * @copyright   Copyright (c), 2021 Amit Roy
+ * @license     MIT public license
+ */
+namespace Inculcate\Collections;
+/**
+ * trait DB.
+ */
+trait DB
+{   
+	
+
+} 
